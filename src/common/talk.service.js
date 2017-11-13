009cc04003c611c16b82155edb3f41d6cdd84380
Nicolas CAVAILLE <niko.cavaille@hotmail.fr>
2017-11-13 18:07:28 +0100

    intégration service speakers

--- src/common/talk.service.js
+++ src/common/talk.service.js
@@ -2,6 +2,10 @@
 export default class TalkService {
 
     findAllSpeakers() {
-        return [{id: 'sp1', fullname:'hello'}];
+        return $.get('http://localhost:3000/speakers')
+    }
+
+    findSpeakerById(id) {
+        return $.get(`http://localhost:3000/speakers/${id}`)
     }
 }

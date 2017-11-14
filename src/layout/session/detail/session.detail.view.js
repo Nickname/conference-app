33cab71f54fcdef73955c3a34aaee70f0550a064
Olivier LE HENAFF <lehenaff.olivier@hotmail.fr>
2017-11-14 16:35:15 +0100

    partie session presque ok

--- src/layout/session/detail/session.detail.view.js
+++ src/layout/session/detail/session.detail.view.js
@@ -7,6 +7,28 @@ export default class SessionDetailView {
     }
 
     render() {
-        
+        {
+            var path = location.hash.split("/")
+            let id = path[path.length - 1]
+    
+            this.talkService.findSessionById(id)
+                .then(s => {
+                    let tabSessionDetail = []
+
+                  
+                       let sessionDetail = session_detail.replace('{{title}}', s.title)
+                        sessionDetail = sessionDetail.replace('{{desc}}', s.desc)
+
+                        let tabPresentateur = []
+                        s.speakers.forEach(sp => {
+                            tabPresentateur.push(`<a href="#speakers-list">${sp}</a>`)
+                        })
+                        session_detail.replace('{{firstname}}', s.firstname, tabPresentateur.join(''))
+                        session_detail.replace('{{lastname}}', s.lastname, tabPresentateur.join(''))
+
+                        tabPresentateur.push(sessionDetail)
+                        $('#main-view').html(tabPresentateur.join(""))
+                })
+        }
     }
 }
